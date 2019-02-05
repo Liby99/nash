b3958785e0dffbd986c5b3104a9a8ac00e17d852
Liby Lee <liby99@icloud.com>
2019-02-05 03:09:19 -0800

    Fixed transform bug. Can render cube

--- src/intern/utility/transform.cpp
+++ src/intern/utility/transform.cpp
@@ -7,13 +7,13 @@ nash::Transform::Transform() :
   rotation(Quaternionf::Identity()),
   position(Vector3f::Zero()),
   scale(Vector3f::Identity()) {
-  // Do Nothing
+  scale << 1, 1, 1;
 }
 
 Matrix4f nash::Transform::getTransform() {
   Matrix4f local = Matrix4f::Zero();
   local.col(3) << position.x(), position.y(), position.z(), 1;
-  local.block(0, 0, 2, 2) = rotation.normalized().toRotationMatrix();
-  local.block(0, 0, 2, 2) *= Scaling(scale);
+  local.topLeftCorner<3, 3>() = rotation.normalized().toRotationMatrix();
+  local.topLeftCorner<3, 3>() *= Scaling(scale);
   return local * world;
 }
