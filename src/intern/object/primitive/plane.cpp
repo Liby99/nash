6152b936acb3ea4ce50be97464ec3fba9f8d08b9
Liby Lee <liby99@icloud.com>
2019-02-07 19:06:23 -0800

    Fixing styles

--- src/intern/object/primitive/plane.cpp
+++ src/intern/object/primitive/plane.cpp
@@ -6,13 +6,13 @@ Plane::Plane() : Mesh() {
   indices = MatrixXu(3, 2);
   indices.col(0) << 0, 1, 3;
   indices.col(1) << 1, 2, 3;
-  
+
   positions = MatrixXf(3, 4);
   positions.col(0) << -0.5, 0, -0.5;
   positions.col(1) << -0.5, 0, 0.5;
   positions.col(2) << 0.5, 0, 0.5;
   positions.col(3) << 0.5, 0, -0.5;
-  
+
   normals = MatrixXf(3, 4);
   normals.col(0) << 0, 1, 0;
   normals.col(1) << 0, 1, 0;
