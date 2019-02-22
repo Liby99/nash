6824c319af26a36edd7f31b0b0c0832ce9d285fa
Liby Lee <liby99@icloud.com>
2019-02-22 10:56:48 -0800

    Done hardcoding coefficients

--- test/sh/env_shading_test.cpp
+++ test/sh/env_shading_test.cpp
@@ -7,7 +7,7 @@
 
 using namespace nash;
 
-const std::string directory = "./image/cubemap/colors/";
+const std::string directory = "./image/cubemap/gracecathedral/";
 const std::string modelFile = "./model/teapot.obj";
 
 const float c1 = 0.429043;
@@ -35,15 +35,20 @@ private:
  * Ravi et al. An Efficient Representation for Irradiance Environment Maps, page 3, section 3.2
  */
 void loadCoefsMatrix(const SHCoefs &l, Matrix4f &mat) {
+  // mat.row(0) << c1 * l.get(2, 2), c1 * l.get(2, -2), c1 * l.get(2, 1), c2 * l.get(1, 1);
+  // mat.row(1) << c1 * l.get(2, -2), -c1 * l.get(2, 2), c1 * l.get(2, -1), c2 * l.get(1, -1);
+  // mat.row(2) << c1 * l.get(2, 1), c1 * l.get(2, -1), c3 * l.get(2, 0), c2 * l.get(1, 0);
+  // mat.row(3) << c2 * l.get(1, 1), c2 * l.get(1, -1), c2 * l.get(1, 0),
+  //     c4 * l.get(0, 0) - c5 * l.get(2, 0);
+  // mat << l.get(0, 0), l.get(1, 1), l.get(2, 2), l.get(3, 3),
+  //        l.get(1, -1), l.get(1, 0), l.get(2, 1), l.get(3, 2),
+  //        l.get(2, -2), l.get(2, -1), l.get(2, 0), l.get(3, 1),
+  //        l.get(3, -3), l.get(3, -2), l.get(3, -1), l.get(3, 0);
   mat.row(0) << c1 * l.get(2, 2), c1 * l.get(2, -2), c1 * l.get(2, 1), c2 * l.get(1, 1);
   mat.row(1) << c1 * l.get(2, -2), -c1 * l.get(2, 2), c1 * l.get(2, -1), c2 * l.get(1, -1);
   mat.row(2) << c1 * l.get(2, 1), c1 * l.get(2, -1), c3 * l.get(2, 0), c2 * l.get(1, 0);
   mat.row(3) << c2 * l.get(1, 1), c2 * l.get(1, -1), c2 * l.get(1, 0),
       c4 * l.get(0, 0) - c5 * l.get(2, 0);
-  // mat.row(0) << l.get(0, 0), l.get(1, 1), l.get(2, 2), l.get(3, 3);
-  // mat.row(1) << l.get(1, -1), l.get(1, 0), l.get(2, 1), l.get(3, 2);
-  // mat.row(2) << l.get(2, -2), l.get(2, -1), l.get(2, 0), l.get(3, 1);
-  // mat.row(3) << l.get(3, -3), l.get(3, -2), l.get(3, -1), l.get(3, 0);
 }
 
 int main(int argc, char *argv[]) {
@@ -64,37 +69,74 @@ int main(int argc, char *argv[]) {
   SkyBox skybox(cubeMap);
   scene.addObject(skybox);
 
-  SkyBoxSHCalculator calc(cubeMap, 4);
+  SkyBoxSHCalculator calc(cubeMap, 3);
   const std::vector<SHCoefs *> &list = calc.getCoefsList();
   SHCoefs &red = *list[0], &green = *list[1], &blue = *list[2];
+
+  // BEGIN TEMP
+  red.set(0, 0, 0.79);
+  red.set(1, -1, 0.39);
+  red.set(1, 0, -.34);
+  red.set(1, 1, -.29);
+  red.set(2, -2, -.11);
+  red.set(2, -1, -.26);
+  red.set(2, 0, -.16);
+  red.set(2, 1, 0.56);
+  red.set(2, 2, 0.21);
+  green.set(0, 0, 0.44);
+  green.set(1, -1, 0.35);
+  green.set(1, 0, -.18);
+  green.set(1, 1, -.06);
+  green.set(2, -2, -.05);
+  green.set(2, -1, -.22);
+  green.set(2, 0, -.09);
+  green.set(2, 1, 0.21);
+  green.set(2, 2, -0.05);
+  blue.set(0, 0, 0.54);
+  blue.set(1, -1, 0.60);
+  blue.set(1, 0, -.27);
+  blue.set(1, 1, 0.01);
+  blue.set(2, -2, -.12);
+  blue.set(2, -1, -.47);
+  blue.set(2, 0, -.15);
+  blue.set(2, 1, .14);
+  blue.set(2, 2, -.30);
+  // END TEMP
+
   Matrix4f redCoefs, greenCoefs, blueCoefs;
   loadCoefsMatrix(red, redCoefs);
   loadCoefsMatrix(green, greenCoefs);
   loadCoefsMatrix(blue, blueCoefs);
 
-  std::cout << redCoefs << std::endl;
+  EnvShading setEnv("set-env", redCoefs, greenCoefs, blueCoefs);
 
   AssimpObject model(modelFile);
   AssimpMesh &mesh = *model.getMeshes()[0];
   mesh.setShader(Shader::get("./shader/env_shading"));
-  EnvShading setEnv("set-env", redCoefs, greenCoefs, blueCoefs);
   mesh.attachScript(setEnv);
-  Looper loopMesh("looper", 0, 4);
+  Looper loopMesh("looper", 0, 5);
   mesh.attachScript(loopMesh);
   scene.addObject(model);
 
+  Sphere sphere;
+  sphere.setShader(Shader::get("./shader/env_shading"));
+  sphere.attachScript(setEnv);
+  Looper loopSphere("looper", 1, 5);
+  sphere.attachScript(loopSphere);
+  scene.addObject(sphere);
+
   SHSphere redSphere(red);
-  Looper loopRed("looper", 1, 4);
+  Looper loopRed("looper", 2, 5);
   redSphere.attachScript(loopRed);
   scene.addObject(redSphere);
 
   SHSphere greenSphere(green);
-  Looper loopGreen("looper", 2, 4);
+  Looper loopGreen("looper", 3, 5);
   greenSphere.attachScript(loopGreen);
   scene.addObject(greenSphere);
 
   SHSphere blueSphere(blue);
-  Looper loopBlue("looper", 3, 4);
+  Looper loopBlue("looper", 4, 5);
   blueSphere.attachScript(loopBlue);
   scene.addObject(blueSphere);
 
--- test/sh/res/shader/env_shading.frag.glsl
+++ test/sh/res/shader/env_shading.frag.glsl
@@ -10,6 +10,8 @@ uniform mat4 redCoefs;
 uniform mat4 greenCoefs;
 uniform mat4 blueCoefs;
 
+#define M_PI 3.1415926535897932384626433832795
+
 float getChannel(mat4 m, vec3 n) {
   vec4 nb = vec4(n, 1);
   return dot(nb, m * nb);
@@ -17,9 +19,9 @@ float getChannel(mat4 m, vec3 n) {
 
 void main() {
   vec3 n = normalize(fragNormal);
-  n = vec3(-n.x, -n.z, n.y);
+  // n = vec3(-n.x, -n.z, n.y);
   float r = getChannel(redCoefs, n);
   float g = getChannel(greenCoefs, n);
   float b = getChannel(blueCoefs, n);
-  finalColor = vec4(r / 3, g / 3, b / 3, 1);
+  finalColor = vec4(r, g, b, 1);
 }
